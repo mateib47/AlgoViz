99e99cf82925f9bf1e166237b298673da53184ea
mateib47 <m.bucur@student.utwente.nl>
2021-10-06 23:35:54 +0200

    Initial commit

---
+++ src/test/java/com/project/AlgoViz/AlgoVizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.AlgoViz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoVizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
