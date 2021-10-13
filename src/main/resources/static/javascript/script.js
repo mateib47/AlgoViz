b13cd54d30abad95dd72423db4f67e3a1e40e950
mateib47 <m.bucur@student.utwente.nl>
2021-10-13 23:58:42 +0200

    started implementing the sorting page; implemented the array generation

--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -3,15 +3,20 @@ import {sequentialSearch, binarySearch} from './algorithms/searching.js';
 let arraySizeValue;
 let toFind;
 const arraySize = document.querySelector('#sorting-size-input');
+
 arraySize.addEventListener('change', event =>{
     arraySizeValue = event.target.value
-    generateArray();
+    if(event.target.id === 'new-array-searching'){
+        generateSearchingArray();
+    }else{
+        generateSortingArray();
+    }
 });
 
-function generateArray(){
+function generateSearchingArray(){
     eraseArray();
     toFind = Math.floor(Math.random() * arraySizeValue)
-    const array = document.querySelector('.array');
+    const array = document.querySelector('#array-searching');
     for(let i=0; i<arraySizeValue;i++){
         if(i == toFind){
             array.innerHTML += '<div class="box green" id="box-'+i+'"></div>';
@@ -20,26 +25,58 @@ function generateArray(){
         }
     }
 }
+function generateSortingArray(){
+    eraseArray();
+    let arr = [];
+    for (let i = 0;i<arraySizeValue;i++){
+        arr[i] = i;
+    }
+    arr = shuffle([...arr]);
+    const array = document.querySelector('#array-sorting');
+    for(let i=0; i<arraySizeValue;i++){
+            array.innerHTML += `<div class="bar" id="${'bar'+arr[i]}" style="max-height:${arr[i]*10}% "></div>`;
+    }
+}
+function shuffle(array){
+    for(let i=0;i<array.length;i++){
+        let rand = Math.floor(Math.random() * (i+1));
+        let temp = array[i];
+        array[i] = array[rand];
+        array[rand] = temp;
+    }
+    console.log(array);
+    return array;
+}
+
 function eraseArray(){
     const array = document.querySelector('.array');
     array.innerHTML = '';
-    document.getElementById('prompt').style.display = 'none';
+    const prompts = document.getElementsByClassName('prompt');
+    Array.from(prompts).forEach(function (elem){
+        elem.style.display = 'none';
+    });
 }
 
 
-const newArray = document.querySelector('#new-array');
-newArray.addEventListener('click', event =>{
-    event.preventDefault();
-    if(!arraySizeValue){
-        alert('Please select the size of the array');
-    }else{
-        eraseArray();
-        generateArray();
-    }
+const newArray = document.getElementsByClassName('new-array');
+Array.from(newArray).forEach(function (element){
+    element.addEventListener('click', event =>{
+        event.preventDefault();
+        if(!arraySizeValue){
+            alert('Please select the size of the array');
+        }else{
+            eraseArray();
+            if(event.target.id === 'new-array-searching'){
+                generateSearchingArray();
+            }else{
+                generateSortingArray();
+            }
+        }
+    });
 });
 
-const form = document.querySelector('#searching-form');
-form.addEventListener('submit', event => {
+const searchingForm = document.querySelector('#searching-form');
+searchingForm.addEventListener('submit', event => {
     event.preventDefault();
     const lengthInput = document.querySelector('#sorting-size-input');
     const algTypeInput = document.querySelector('#searching-alg');
@@ -53,3 +90,4 @@ form.addEventListener('submit', event => {
       }
     }
 });
+
