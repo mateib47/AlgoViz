a1264a3e64305d0c9deb3fddc4da9e308b2c1ce4
mateib47 <m.bucur@student.utwente.nl>
2021-10-16 22:36:21 +0200

    started implementing the visualization mechanism for bubblesort; added the description for it

--- src/main/resources/static/javascript/algorithms/sorting.js
+++ src/main/resources/static/javascript/algorithms/sorting.js
@@ -1,27 +1,64 @@
 function bubbleSort(length){
     let sorted = false;
-    for(let i = 0;i<length;i++){
-        //console.log(document.querySelector('#bar-'+i));
-    }
+    let count = 0;
     while(!sorted){
         sorted = true;
         for(let i = 0;i< length-1;i++){
-            let a = document.querySelector('#bar-'+i);
-            let b = document.querySelector('#bar-'+`${i+1}`)
-            if((parseFloat(a.style.maxHeight) / 100.0)>(parseFloat(b.style.maxHeight) / 100.0)){
-                sorted = false
-                //console.log(a.style.maxHeight.valueOf() +'>' +b.style.maxHeight.valueOf());
-                let temp = a.style.maxHeight;
-                a.style.maxHeight = b.style.maxHeight;
-                b.style.maxHeight = temp;
+            if(!task(count, length)){
+                sorted = false;
             }
+            count++;
         }
-    }
-    for(let i = 0;i<length;i++){
-        //console.log(document.querySelector('#bar-'+i));
     }
 }
 
+function task(count, length) {
+    console.log(count);
+    const i = count % length;
+    let sorted = true;
+    setTimeout(function() {
+        let a = document.querySelector('#bar-'+i);
+        let b = document.querySelector('#bar-'+`${i+1}`);
+        if((parseFloat(a.style.maxHeight) / 100.0)>(parseFloat(b.style.maxHeight) / 100.0)){
+            sorted = false;
+            let temp = a.style.maxHeight;
+            a.style.maxHeight = b.style.maxHeight;
+            b.style.maxHeight = temp;
+        }
+    }, 300 * count);
+    return sorted;
+}//fix it using callback functions probably; the bubblesort function does not wait for task() to return the value.
+/*
+function bubbleSort(length){
+    let sorted = false;
+    let counter = 0;
+    while(!sorted){
+            sorted = true;
+        for(let i = 0;i< length-1;i++){
+            counter++;
+            let a = document.querySelector('#bar-'+i);
+            let b = document.querySelector('#bar-'+`${i+1}`);
+            if((parseFloat(a.style.maxHeight) / 100.0)>(parseFloat(b.style.maxHeight) / 100.0)){
+                sorted = false;
+                    let temp = a.style.maxHeight;
+                    a.style.maxHeight = b.style.maxHeight;
+                    b.style.maxHeight = temp;
+            }
+        }
+}
+}
+
+function delay(i) {
+    let a = document.querySelector('#bar-'+i);
+    let b = document.querySelector('#bar-'+`${i+1}`);
+    setTimeout(function() {
+        console.log(i);
+        let temp = a.style.maxHeight;
+        a.style.maxHeight = b.style.maxHeight;
+        b.style.maxHeight = temp;
+    }, 300 * i);
+}
+*/
 function quickSort(length){
 
 }
@@ -35,3 +72,33 @@ function heapSort(length){
 }
 
 export {bubbleSort, quickSort, mergeSort, heapSort};
+/*
+function bubbleSort(length){
+    let sorted = false;
+    for(let i = 0;i<length;i++){
+        //console.log(document.querySelector('#bar-'+i));
+    }
+    while(!sorted){
+        sorted = true;
+        for(let i = 0;i< length-1;i++){
+            sorted = task(i);
+
+        }
+    }
+}
+
+function task(i) {
+    let sorted = true;
+    setTimeout(function() {
+        let a = document.querySelector('#bar-'+i);
+        let b = document.querySelector('#bar-'+`${i+1}`);
+        if((parseFloat(a.style.maxHeight) / 100.0)>(parseFloat(b.style.maxHeight) / 100.0)){
+            sorted = false;
+            let temp = a.style.maxHeight;
+            a.style.maxHeight = b.style.maxHeight;
+            b.style.maxHeight = temp;
+        }
+        }, 300 * i);
+    return sorted;
+}
+ */
