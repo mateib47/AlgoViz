2ceb7bdf38b1acf06db0d1e552dcc52c80e7100c
mateib47 <m.bucur@student.utwente.nl>
2021-10-18 23:59:27 +0200

    implemented merge sort algorithm

--- src/main/resources/static/javascript/algorithms/sorting.js
+++ src/main/resources/static/javascript/algorithms/sorting.js
@@ -27,11 +27,38 @@ function quickSort(length){
 }
 
 function mergeSort(length){
-
+    let array = [];
+    let elements = [];
+    for(let i=0;i<length;i++){
+        array.push({value:parseFloat(document.querySelector('#bar-'+`${i}`).style.maxHeight)/100.0, id: i});
+    }
+    console.log(mergeSorting(array));
+}
+function mergeSorting(array){
+    const half = array.length / 2;
+    if(array.length < 2){
+        return array;
+    }
+    const left = array.splice(0, half);
+    return merge(mergeSorting(left), mergeSorting(array));
+}
+function merge(left, right){
+    let arr = [];
+    while(left.length && right.length) {
+        if (left[0].value < right[0].value) {
+            arr.push(left.shift());
+        } else {
+            arr.push(right.shift());
+        }
+    }
+    return [...arr, ...left, ...right];
 }
 
 function heapSort(length){
 
+}
+function sortingDone(){
+    alert('Done');
 }
 
 export {bubbleSort, quickSort, mergeSort, heapSort};
