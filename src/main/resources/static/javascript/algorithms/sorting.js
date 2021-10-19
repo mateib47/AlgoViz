96ffb328f318ca3575cb4b24c89c194b24b92dc2
mateib47 <m.bucur@student.utwente.nl>
2021-10-19 20:20:46 +0200

    partially working visualization for merge sort

--- src/main/resources/static/javascript/algorithms/sorting.js
+++ src/main/resources/static/javascript/algorithms/sorting.js
@@ -25,12 +25,11 @@ function task(count, length, i) {
 function quickSort(length){
 
 }
-
+let mergeCount = 0;
 function mergeSort(length){
     let array = [];
-    let elements = [];
     for(let i=0;i<length;i++){
-        array.push({value:parseFloat(document.querySelector('#bar-'+`${i}`).style.maxHeight)/100.0, id: i});
+        array.push({value:parseFloat(document.querySelector('#bar-'+`${i}`).style.maxHeight)/100.0, id: i, percent: document.querySelector('#bar-'+`${i}`).style.maxHeight});
     }
     console.log(mergeSorting(array));
 }
@@ -43,18 +42,40 @@ function mergeSorting(array){
     return merge(mergeSorting(left), mergeSorting(array));
 }
 function merge(left, right){
+    mergeCount++;
     let arr = [];
     while(left.length && right.length) {
+        let val;
         if (left[0].value < right[0].value) {
-            arr.push(left.shift());
+            val = left.shift();
+            arr.push(val);
         } else {
-            arr.push(right.shift());
+            val = right.shift();
+            arr.push(val);
         }
     }
-    return [...arr, ...left, ...right];
+    const result = [...arr, ...left, ...right];
+    const min = Math.min(...result.map(elem => elem.id))
+    for (let i=0; i< result.length;i++){
+        result[i].id = i+min;
+    }
+    mergeTask(result);
+    return result;
 }
+function mergeTask(array) {
+    console.log(mergeCount);
+    setTimeout(function() {
+        for(let e of array){
+            document.querySelector('#bar-'+e.id).style.maxHeight = e.percent;
+        }
+    }, 300*mergeCount);
+}
+function delay(i) {
+    setTimeout(function () {
 
-function heapSort(length){
+    }, 300 * i);
+}
+function heapSort(){
 
 }
 function sortingDone(){
