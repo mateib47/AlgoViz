0f23b755a044573ec90c26179f96527ec8b0f0fd
Matei <m.bucur@student.utwente.nl>
2021-11-30 17:52:56 +0100

    add bfs visualisation

--- src/main/resources/static/javascript/algorithms/searching.js
+++ src/main/resources/static/javascript/algorithms/searching.js
@@ -85,15 +85,16 @@ function elementFound(found, depth){
 }
 
 function bfs(start, goal, m){
+    let steps = [];
     let matrix = [...m];
     let directions = [[-1,0],[0,1],[1,0],[0,-1]];
     let frontier = [];
     frontier.push(start);
     while(frontier.length > 0){
+        let neighbours = [];
         let node = frontier.shift();
         let x = node[0];
         let y = node[1];
-        console.log(matrix[x][y])
         if (matrix[x][y] == 2){
             console.log("success");
             break;
@@ -106,10 +107,28 @@ function bfs(start, goal, m){
                 if(next == 0){
                     matrix[xn][yn] = -2;
                     frontier.push([xn,yn]);
+                    neighbours.push([xn,yn]);
                 }else if(next == 2){
                     frontier.push([xn,yn]);
                 }
             }
         } 
+        steps.push(neighbours);
+    }
+    vizualise(steps);
+}
+
+function vizualise(steps){
+    for(let i=0; i < steps.length;i++){
+        setTimeout(function () {
+            for(let j=0; j < steps[i].length; j++){
+                let x = steps[i][j][0];
+                let y = steps[i][j][1];
+                document.querySelector('#box-'+x+'-'+y).style.backgroundColor = '#F8DE7E';
+                setTimeout(function () {
+                    document.querySelector('#box-'+x+'-'+y).style.backgroundColor = '#4D8C57';
+                }, 10 * i);
+            }
+        }, 20 * i);
     }
 }
